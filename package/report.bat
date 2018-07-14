8246ca6cd9abab30e8a5e1731db8afab254fec27
Sekar <prsekar@deloitte.com>
2018-07-14 17:33:02 +0530

    comitting package

---
+++ package/report.bat
@@ -0,0 +1,10 @@
+
+@ECHO OFF
+
+java -jar git.parser.jar
+
+
+C:\Users\prsekar\Desktop\RI\softwares\IDE\eclipse-jee-oxygen-1-win32-x86_64\eclipse\eclipsec.exe  -data C:\Users\prsekar\Desktop\cf\new_code\ -nosplash -application HeadlessAnalyzer.myhead C:\\Users\\prsekar\\Desktop\\cf\\Disney\\storage\\changeSet.csv C:\\Users\\prsekar\\Desktop\\cf\\Disney\\Headless\\mails.properties
+
+
+pause >nul
